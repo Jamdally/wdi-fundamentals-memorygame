d58df5ddff8d948627c6be2ea6fd29115336fd79
Jessica Dally <jessicadally@Jessicas-MacBook-Pro.local>
2018-01-15 18:35:19 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,36 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "king";
-var cardFour = "queen";
+//Create an array for the cards
+var cards = ['queen', 'queen', 'king', 'king'];
+
+//Create an array for the card(s) in Play
+var cardsInPlay = [];
+
+//Create variable to represent the first card the user flipped
+var cardOne = cards[0];
+// Add card to array of cards in play using push method
+cardsInPlay.push(cardOne);
+// Use console.log() to display the card that the user flipped
 console.log("User flipped " + cardOne);
+
+// Create a variable to represent the second card the user flipped
+var cardTwo = cards[2];
+// Add card to array of cards in play using push method
+cardsInPlay.push(cardTwo);
+// Use console.log() to display the card that the user flipped
 console.log("User flipped " + cardTwo);
+
+// Check to see if the length of the cardsInPlay array is 2
+if (cardsInPlay.length === 2) {
+
+// Check to see if two cards match and provide feedback
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+	} 
+else {
+    alert("Sorry, try again.");
+  	}
+}
+
+
+// Use console.log to display card selected by User (user flipped) two at a time. 
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
